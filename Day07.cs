9e69ed7f7080f873f737f6f2840d796d54705f20
Karel Lannoy <karel.lannoy@gmail.com>
2022-12-08 09:29:03 +0100

    Day07 & Day08

---
+++ Day07.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public static class Day07
+    {
+
+        public static void Part1()
+        {
+            List<Directory> directoryList = new List<Directory>();
+            Directory root = new Directory();
+            root.Name = "/";
+            directoryList.Add(root);
+            Directory currentDir = null;
+            var input = File.ReadAllLines(@"Inputs/Input07.txt");
+            foreach (var command in input)
+            {
+                if (command.StartsWith("$ cd"))
+                {
+                    var dirName = command.Substring(5);
+                    if (dirName == "/")
+                    {
+                        currentDir = root;
+                        continue;
+                    }
+                    if (dirName == "..")
+                    {
+                        currentDir = currentDir.Parent;
+                        continue;
+                    }
+                    if (currentDir.Directories.Any(d => d.Name == dirName))
+                        currentDir = currentDir.Directories.First(d => d.Name == dirName);
+                    else
+                    {
+                        Directory newDir = new Directory();
+                        newDir.Name = dirName;
+                        newDir.Parent = currentDir;
+                        currentDir.Directories.Add(newDir);
+                        currentDir = newDir;
+                        directoryList.Add(newDir);
+                    }
+                    continue;
+                }
+                if (command.StartsWith("$ ls"))
+                    continue;
+                if (command.StartsWith("dir"))
+                {
+                    var dirName = command.Split(" ")[1];
+                    if (!currentDir.Directories.Any(d => d.Name == dirName))
+                    {
+                        Directory newDir = new Directory();
+                        newDir.Name = dirName;
+                        newDir.Parent = currentDir;
+                        currentDir.Directories.Add(newDir);
+                        directoryList.Add(newDir);
+                    }
+                    continue;
+                }
+                var fileParts = command.Split(" ");
+                currentDir.Files.Add(new Tuple<string, long>(fileParts[1], long.Parse(fileParts[0])));
+            }
+
+            var totalSize = CalculateSize(root);
+            Console.WriteLine(directoryList.Where(d => d.Size <= 100000).Sum(d => d.Size));
+
+        }
+
+        private static long CalculateSize(Directory dir)
+        {
+            long size = dir.Files.Sum(t => t.Item2);
+            foreach (var child in dir.Directories)
+            {
+                size+= CalculateSize(child);
+            }
+            dir.Size = size;
+            return size;
+        }
+
+        public static void Part2()
+        {
+            List<Directory> directoryList = new List<Directory>();
+            Directory root = new Directory();
+            root.Name = "/";
+            directoryList.Add(root);
+            Directory currentDir = null;
+            var input = File.ReadAllLines(@"Inputs/Input07.txt");
+            foreach (var command in input)
+            {
+                if (command.StartsWith("$ cd"))
+                {
+                    var dirName = command.Substring(5);
+                    if (dirName == "/")
+                    {
+                        currentDir = root;
+                        continue;
+                    }
+                    if (dirName == "..")
+                    {
+                        currentDir = currentDir.Parent;
+                        continue;
+                    }
+                    if (currentDir.Directories.Any(d => d.Name == dirName))
+                        currentDir = currentDir.Directories.First(d => d.Name == dirName);
+                    else
+                    {
+                        Directory newDir = new Directory();
+                        newDir.Name = dirName;
+                        newDir.Parent = currentDir;
+                        currentDir.Directories.Add(newDir);
+                        currentDir = newDir;
+                        directoryList.Add(newDir);
+                    }
+                    continue;
+                }
+                if (command.StartsWith("$ ls"))
+                    continue;
+                if (command.StartsWith("dir"))
+                {
+                    var dirName = command.Split(" ")[1];
+                    if (!currentDir.Directories.Any(d => d.Name == dirName))
+                    {
+                        Directory newDir = new Directory();
+                        newDir.Name = dirName;
+                        newDir.Parent = currentDir;
+                        currentDir.Directories.Add(newDir);
+                        directoryList.Add(newDir);
+                    }
+                    continue;
+                }
+                var fileParts = command.Split(" ");
+                currentDir.Files.Add(new Tuple<string, long>(fileParts[1], long.Parse(fileParts[0])));
+            }
+
+            var totalSize = CalculateSize(root);
+
+            var freeSpace = 70000000 - root.Size;
+            var neededSpaceToClear = 30000000 - freeSpace;
+
+            Console.WriteLine(directoryList.Where(d => d.Size > neededSpaceToClear).Min(d => d.Size));
+
+
+        }
+
+        private class Directory
+        {
+            public string Name { get; set; }
+            public List<Tuple<string, long>> Files { get; set; }
+            public List<Directory> Directories { get; set; }
+            public Directory Parent { get; set; }
+            public long Size { get; set; }
+
+            public Directory()
+            {
+                Files = new List<Tuple<string, long>>();
+                Directories = new List<Directory>();
+            }
+        }
+    }
+
+
+}
---
+++ Day08.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public static class Day08
+    {
+
+        public static void Part1()
+        {
+            var matrix = File.ReadAllLines(@"Inputs/Input08.txt").ToList();
+            var visibleTreeCounter = 0;
+
+            for (int y = 0; y < matrix.Count; y++)
+            {
+                for (int x = 0; x < matrix[y].Length; x++)
+                {
+                    if (IsTreeVisible(int.Parse(matrix[y][x].ToString()), x, y, matrix))
+                    {
+                        visibleTreeCounter++;
+                    }
+                }
+            }
+            Console.WriteLine(visibleTreeCounter.ToString());
+        }
+
+        private static bool IsTreeVisible(int tree, int posx, int posy, List<string> matrix)
+        {
+            //LookLeft
+            var left = true;
+            for (int i = posx - 1; i >= 0; i--)
+            {
+                if (int.Parse(matrix[posy][i].ToString()) >= tree)
+                {
+                    left = false;
+                }
+            }
+            //LookRight
+            var right = true;
+            for (int i = posx + 1; i < matrix[posy].Length; i++)
+            {
+                if (int.Parse(matrix[posy][i].ToString()) >= tree)
+                {
+                    right = false;
+                }
+            }
+            //LookUp
+            var up = true;
+            for (int i = posy - 1; i >= 0; i--)
+            {
+                if (int.Parse(matrix[i][posx].ToString()) >= tree)
+                {
+                    up = false;
+                }
+            }
+            //LookDown
+            var down = true;
+            for (int i = posy + 1; i < matrix.Count; i++)
+            {
+                if (int.Parse(matrix[i][posx].ToString()) >= tree)
+                {
+                    down = false;
+                }
+            }
+
+            return left || right || up || down;
+        }
+
+        public static void Part2()
+        {
+            var matrix = File.ReadAllLines(@"Inputs/Input08.txt").ToList();
+            var highestScenicScore = 0;
+
+            for (int y = 0; y < matrix.Count; y++)
+            {
+                for (int x = 0; x < matrix[y].Length; x++)
+                {
+                    var scenicScore = CalculateScenicScore(int.Parse(matrix[y][x].ToString()), x, y, matrix);
+                    if (scenicScore > highestScenicScore)
+                        highestScenicScore = scenicScore;
+                }
+            }
+            Console.WriteLine(highestScenicScore.ToString());
+        }
+
+        private static int CalculateScenicScore(int tree, int posx, int posy, List<string> matrix)
+        {
+            if (posx==0 || posy == 0 || posx == matrix[posy].Length - 1 || posy == matrix.Count - 1)
+            {
+                return 0;
+            }
+
+            //LookLeft
+            int left = 0;
+            for (int i = posx - 1; i >= 0; i--)
+            {
+                left++;
+                if (int.Parse(matrix[posy][i].ToString()) >= tree)
+                {
+                    break;
+                }
+            }
+            //LookRight
+            int right = 0;
+            for (int i = posx + 1; i < matrix[posy].Length; i++)
+            {
+                right++;
+                if (int.Parse(matrix[posy][i].ToString()) >= tree)
+                {
+                    break;
+                }
+            }
+            //LookUp
+            int up = 0;
+            for (int i = posy - 1; i >= 0; i--)
+            {
+                up++;
+                if (int.Parse(matrix[i][posx].ToString()) >= tree)
+                {
+                    break;
+                }
+            }
+            //LookDown
+            int down = 0;
+            for (int i = posy + 1; i < matrix.Count; i++)
+            {
+                down++;
+                if (int.Parse(matrix[i][posx].ToString()) >= tree)
+                {
+                    break;
+                }
+            }
+
+            return left * right * up * down;
+        }
+    }
+
+
+}
