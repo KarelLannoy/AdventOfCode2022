266572c36d56d6bd41ed7c5291ca3849af5f5656
Karel Lannoy <karel.lannoy@gmail.com>
2022-12-09 08:42:14 +0100

    Day09

--- Day08.cs
+++ Day08.cs
@@ -8,12 +8,10 @@ namespace AdventOfCode2022
 {
     public static class Day08
     {
-
         public static void Part1()
         {
             var matrix = File.ReadAllLines(@"Inputs/Input08.txt").ToList();
             var visibleTreeCounter = 0;
-
             for (int y = 0; y < matrix.Count; y++)
             {
                 for (int x = 0; x < matrix[y].Length; x++)
@@ -29,6 +27,11 @@ namespace AdventOfCode2022
 
         private static bool IsTreeVisible(int tree, int posx, int posy, List<string> matrix)
         {
+            //Edge always visible
+            if (posx == 0 || posy == 0 || posx == matrix[posy].Length - 1 || posy == matrix.Count - 1)
+            {
+                return true;
+            }
             //LookLeft
             var left = true;
             for (int i = posx - 1; i >= 0; i--)
@@ -36,6 +39,7 @@ namespace AdventOfCode2022
                 if (int.Parse(matrix[posy][i].ToString()) >= tree)
                 {
                     left = false;
+                    break;
                 }
             }
             //LookRight
@@ -45,6 +49,7 @@ namespace AdventOfCode2022
                 if (int.Parse(matrix[posy][i].ToString()) >= tree)
                 {
                     right = false;
+                    break;
                 }
             }
             //LookUp
@@ -54,6 +59,7 @@ namespace AdventOfCode2022
                 if (int.Parse(matrix[i][posx].ToString()) >= tree)
                 {
                     up = false;
+                    break;
                 }
             }
             //LookDown
@@ -63,9 +69,9 @@ namespace AdventOfCode2022
                 if (int.Parse(matrix[i][posx].ToString()) >= tree)
                 {
                     down = false;
+                    break;
                 }
             }
-
             return left || right || up || down;
         }
 
@@ -73,7 +79,6 @@ namespace AdventOfCode2022
         {
             var matrix = File.ReadAllLines(@"Inputs/Input08.txt").ToList();
             var highestScenicScore = 0;
-
             for (int y = 0; y < matrix.Count; y++)
             {
                 for (int x = 0; x < matrix[y].Length; x++)
@@ -88,11 +93,11 @@ namespace AdventOfCode2022
 
         private static int CalculateScenicScore(int tree, int posx, int posy, List<string> matrix)
         {
+            //Disregard edge
             if (posx==0 || posy == 0 || posx == matrix[posy].Length - 1 || posy == matrix.Count - 1)
             {
                 return 0;
             }
-
             //LookLeft
             int left = 0;
             for (int i = posx - 1; i >= 0; i--)
@@ -133,10 +138,7 @@ namespace AdventOfCode2022
                     break;
                 }
             }
-
             return left * right * up * down;
         }
     }
-
-
 }
---
+++ Day09.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public static class Day09
+    {
+        public static void Part1()
+        {
+            var head = new Point(0, 0);
+            var tail = new Point(0, 0);
+            Dictionary<Point, int> visited = new Dictionary<Point, int>();
+            visited.Add(tail, 1);
+            var instructions = File.ReadAllLines(@"Inputs/Input09.txt").Select(l => new Tuple<string, int>(l.Split(" ")[0], int.Parse(l.Split(" ")[1]))).ToList();
+            foreach (var instruction in instructions)
+            {
+                for (int i = 0; i < instruction.Item2; i++)
+                {
+                    switch (instruction.Item1)
+                    {
+                        case "R":
+                            head.X += 1;
+                            break;
+                        case "U":
+                            head.Y += 1;
+                            break;
+                        case "L":
+                            head.X -= 1;
+                            break;
+                        case "D":
+                            head.Y -= 1;
+                            break;
+                    }
+                    tail = LetTailFollowHead(tail, head, visited);
+                }
+            }
+            Console.WriteLine(visited.Count());
+        }
+
+        private static Point LetTailFollowHead(Point tail, Point head, Dictionary<Point, int> visited)
+        {
+            var difX = Math.Abs(head.X - tail.X);
+            var difY = Math.Abs(head.Y - tail.Y);
+            if (difX > 1 || difY > 1)
+            {
+                //Move Tail
+                if (difY == 0 || (difX != 0 && difY != 0))
+                {
+                    if (head.X - tail.X > 0)
+                        tail.X++;
+                    else
+                        tail.X--;
+                }
+                if (difX == 0 || (difX != 0 && difY != 0))
+                {
+                    if (head.Y - tail.Y > 0)
+                        tail.Y++;
+                    else
+                        tail.Y--;
+                }
+
+                if (visited != null)
+                {
+                    if (visited.ContainsKey(tail))
+                        visited[tail]++;
+                    else
+                        visited.Add(tail, 1);
+                }
+            }
+            return tail;
+        }
+
+        public static void Part2()
+        {
+            var knotList = new List<Point>();
+            for (int i = 0; i < 10; i++)
+            {
+                knotList.Add(new Point());
+            }
+            var tail = knotList[9];
+            Dictionary<Point, int> visited = new Dictionary<Point, int>();
+            visited.Add(tail, 1);
+            var instructions = File.ReadAllLines(@"Inputs/Input09.txt").Select(l => new Tuple<string, int>(l.Split(" ")[0], int.Parse(l.Split(" ")[1]))).ToList();
+            foreach (var instruction in instructions)
+            {
+                for (int i = 0; i < instruction.Item2; i++)
+                {
+                    switch (instruction.Item1)
+                    {
+                        case "R":
+                            knotList[0] = new Point(knotList[0].X + 1, knotList[0].Y);
+                            break;
+                        case "U":
+                            knotList[0] = new Point(knotList[0].X, knotList[0].Y + 1);
+                            break;
+                        case "L":
+                            knotList[0] = new Point(knotList[0].X - 1, knotList[0].Y);
+                            break;
+                        case "D":
+                            knotList[0] = new Point(knotList[0].X, knotList[0].Y - 1);
+                            break;
+                    }
+                    for (int y = 1; y < knotList.Count; y++)
+                    {
+                        knotList[y] = LetTailFollowHead(knotList[y], knotList[y - 1], y == knotList.Count - 1 ? visited : null);
+                    }
+                }
+            }
+            Console.WriteLine(visited.Count());
+        }
+    }
+}
